15093ec1e971d6564841a7ede41fafe38821a113
Yutaro Tsuji <ytsuji91@gmail.com>
2019-01-07 00:39:02 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,7 +1,7 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
-    puts "I LOVE YOU TOO PUMPKIN!"
+    "I LOVE YOU TOO PUMPKIN!"
   elsif phrase.upcase == phrase
     puts "NO, NOT SINCE 1938!"
   else 
